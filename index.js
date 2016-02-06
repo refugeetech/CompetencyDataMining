2330ab96343816dba04d2ad0a5db104eaed8ec7b
ilix <me@ilix.se>
2016-02-06 15:20:02 +0100

    Initial commit, setup project and added logic for saving courses.

---
+++ index.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+var xml2js = require('xml2js');
+var elastic = require('./lib/elastic');
+
+var parser = new xml2js.Parser();
+
+elastic.connect();
+console.log('(ElasticSearch) Trying to connect...');
+elastic.on('connect', function() {
+  console.log('ES connected');
+  fs.readFile('/Users/ilix/skolverket/amnen_och_kurser.xml', function(err, data) {
+      parser.parseString(data, function (err, result) {
+          //console.dir(result.SubjectsAndCourses);
+          result.SubjectsAndCourses.subject.map(function (s) {
+            var subject = {
+              "code": s.code[0],
+              "name": s.name[0],
+              "courses": []
+            };
+
+            s.courses.map(function (c) {
+              subject.courses.push({
+                "code": c.code[0],
+                "name": c.name[0]
+              });
+            });
+
+            //console.log('Subject: ', subject);
+            elastic.saveSubject(subject);
+          });
+      });
+  });
+});
