e865f9ee83d6805b2b1e335b809aa9cf9abf1633
ilix <me@ilix.se>
2016-02-07 13:42:26 +0100

    Changed index name and stuff

--- index.js
+++ index.js
@@ -8,19 +8,22 @@ var parser = new xml2js.Parser();
 elastic.connect();
 console.log('(ElasticSearch) Trying to connect...');
 
-var saveYrkeTree = function (treeId) {
+var saveYrkeTree = function (treeId, treeName) {
   elastic.save({
     'id': treeId,
     'foralder': 0,
-    'namn': 'Administration, Ekonomi och Juridik'
-  }, 'yrken', 'yrke');
+    'namn': treeName
+  }, 'proficiencies', 'proficiency');
   afClient.doRequest('/af/v0/platsannonser/soklista/yrkesgrupper?yrkesomradeid=' + treeId, function (tree) {
     tree.soklista.sokdata.map(function (data) {
       data.foralder = treeId;
-      elastic.save(data, 'yrken', 'yrke');
-      afClient.doRequest('/af/v0/platsannonser/soklista/yrken?yrkesgruppid=' + data.id, function (yrke) {
-        yrke.foralder = data.id;
-        elastic.save(yrke, 'yrken', 'yrke');
+
+      elastic.save(data, 'proficiencies', 'proficiency');
+      afClient.doRequest('/af/v0/platsannonser/soklista/yrken?yrkesgruppid=' + data.id, function (yrken) {
+        yrken.soklista.sokdata.map(function (yrke) {
+          yrke.foralder = data.id;
+          elastic.save(yrke, 'proficiencies', 'proficiency');
+        });
       });
     });
   });
@@ -48,7 +51,7 @@ elastic.on('connect', function () {
               });
             });
 
-            elastic.saveSubject(subject);
+            elastic.save(subject, 'subjects', 'subject');
           });
       });
   });
@@ -63,16 +66,16 @@ elastic.on('connect', function () {
        "name": languages[k].name,
        "nativeName": languages[k].nativeName
      };
-     elastic.saveLanguage(language);
+     elastic.save(language, 'languages', 'language');
    });
 
    /*
     * Administration, Ekonomi och Juridik.
     */
-   saveYrkeTree(1);
+   saveYrkeTree(1, 'Administration, Ekonomi och Juridik');
 
    /*
     * Hälso- och sjukvård.
     */
-   saveYrkeTree(8);
+   saveYrkeTree(8, 'Hälso-/sjukvård');
 });
