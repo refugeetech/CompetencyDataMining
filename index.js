cc0e0334db2a42369fff674ead1b010d34cd1264
Alexander Czigler <me@ilix.se>
2016-02-08 19:28:31 +0100

    Clarified code.

--- index.js
+++ index.js
@@ -5,57 +5,57 @@ var skolverket = require('./lib/skolverket')
 elastic.connect()
 console.log('(ElasticSearch) Trying to connect...')
 
-var saveYrkeTree = function (treeId, treeName) {
+var saveYrkeTree = (treeId, treeName) => {
   elastic.save({
     'id': treeId,
     'foralder': 0,
     'namn': treeName
-  }, 'proficiencies', 'proficiency');
-  afClient.doRequest('/af/v0/platsannonser/soklista/yrkesgrupper?yrkesomradeid=' + treeId, function (tree) {
-    tree.soklista.sokdata.map(function (data) {
-      data.foralder = treeId;
+  }, 'proficiencies', 'proficiency')
+  afClient.doRequest('/af/v0/platsannonser/soklista/yrkesgrupper?yrkesomradeid=' + treeId, tree => {
+    tree.soklista.sokdata.map(data => {
+      data.foralder = treeId
 
-      elastic.save(data, 'proficiencies', 'proficiency');
-      afClient.doRequest('/af/v0/platsannonser/soklista/yrken?yrkesgruppid=' + data.id, function (yrken) {
-        yrken.soklista.sokdata.map(function (yrke) {
-          yrke.foralder = data.id;
-          elastic.save(yrke, 'proficiencies', 'proficiency');
-        });
-      });
-    });
-  });
-};
+      elastic.save(data, 'proficiencies', 'proficiency')
+      afClient.doRequest('/af/v0/platsannonser/soklista/yrken?yrkesgruppid=' + data.id, yrken => {
+        yrken.soklista.sokdata.map(yrke => {
+          yrke.foralder = data.id
+          elastic.save(yrke, 'proficiencies', 'proficiency')
+        })
+      })
+    })
+  })
+}
 
-elastic.on('connect', function () {
+elastic.on('connect', () => {
   console.log('ES connected')
 
   /*
-   * SubjectsAndCourses
+   * SubjectsAndCourses (Skolverket).
    */
-   skolverket.getSubjects(subject => {
-     elastic.save(subject, 'subjects', 'subject')
-   })
+  skolverket.getSubjects(subject => {
+    elastic.save(subject, 'subjects', 'subject')
+  })
 
   /*
-   * Languages
+   * Languages.
    */
-  var languages = require('./data/languages.json');
-  Object.keys(languages).map(function (k) {
-     var language = {
-       "key": k,
-       "name": languages[k].name,
-       "nativeName": languages[k].nativeName
-     };
-     elastic.save(language, 'languages', 'language');
-   });
+  var languages = require('./data/languages.json')
+  Object.keys(languages).map(k => {
+    var language = {
+      'key': k,
+      'name': languages[k].name,
+      'nativeName': languages[k].nativeName
+    }
+    elastic.save(language, 'languages', 'language')
+  })
 
    /*
     * Administration, Ekonomi och Juridik.
     */
-   saveYrkeTree(1, 'Administration, Ekonomi och Juridik');
+  saveYrkeTree(1, 'Administration, Ekonomi och Juridik')
 
    /*
     * Hälso- och sjukvård.
     */
-   saveYrkeTree(8, 'Hälso-/sjukvård');
-});
+  saveYrkeTree(8, 'Hälso-/sjukvård')
+})
