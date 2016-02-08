d59a90c863182b9d5231f9d5907f45bf66730a3d
Alexander Czigler <me@ilix.se>
2016-02-08 19:24:27 +0100

    Refactored main import, moved Skolverket-related code into its own module.

--- data/get_yrkesomraden.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-curl -H "Accept-Language: json" http://api.arbetsformedlingen.se/af/v0/platsannonser/soklista/yrkesomraden > yrkesomraden.json
-
-curl -H "Accept-Language: json" http://api.arbetsformedlingen.se/af/v0/platsannonser/soklista/yrkesgrupper?yrkesomradeid=1 > adminEkonomiJuridik.json
-curl -H "Accept-Language: json" http://api.arbetsformedlingen.se/af/v0/platsannonser/soklista/yrkesgrupper?yrkesomradeid=8 > halsoSjukvard.json
-
-curl -H "Accept-Language: json" http://api.arbetsformedlingen.se/af/v0/platsannonser/soklista/yrkesgrupper?yrkesomradeid=1 > yrken_adminEkonomiJuridik.json
-curl -H "Accept-Language: json" http://api.arbetsformedlingen.se/af/v0/platsannonser/soklista/yrkesgrupper?yrkesomradeid=8 > yrken_halsoSjukvard.json
--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-var fs = require('fs');
-var xml2js = require('xml2js');
-var elastic = require('./lib/elastic');
-var afClient = require('./lib/afClient');
+var elastic = require('./lib/elastic')
+var afClient = require('./lib/afClient')
+var skolverket = require('./lib/skolverket')
 
-var parser = new xml2js.Parser();
-
-elastic.connect();
-console.log('(ElasticSearch) Trying to connect...');
+elastic.connect()
+console.log('(ElasticSearch) Trying to connect...')
 
 var saveYrkeTree = function (treeId, treeName) {
   elastic.save({
@@ -30,31 +27,14 @@ var saveYrkeTree = function (treeId, treeName) {
 };
 
 elastic.on('connect', function () {
-  console.log('ES connected');
+  console.log('ES connected')
 
   /*
    * SubjectsAndCourses
    */
-  fs.readFile(process.env.COURSES || '/Users/ilix/skolverket/amnen_och_kurser.xml', function (err, data) {
-    parser.parseString(data, function (err, result) {
-          result.SubjectsAndCourses.subject.map(function (s) {
-            var subject = {
-              "code": s.code[0],
-              "name": s.name[0],
-              "courses": []
-            };
-
-            s.courses.map(function (c) {
-              subject.courses.push({
-                "code": c.code[0],
-                "name": c.name[0]
-              });
-            });
-
-            elastic.save(subject, 'subjects', 'subject');
-          });
-      });
-  });
+   skolverket.getSubjects(subject => {
+     elastic.save(subject, 'subjects', 'subject')
+   })
 
   /*
    * Languages
