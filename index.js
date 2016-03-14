382e3a869077afce34931cb898e925f42cb1e194
Alexander Czigler <me@ilix.se>
2016-03-14 20:05:40 +0100

    Reduced import to first level yrkesområden.

--- index.js
+++ index.js
@@ -14,14 +14,16 @@ var saveYrkeTree = (treeId, treeName) => {
   afClient.doRequest('/af/v0/platsannonser/soklista/yrkesgrupper?yrkesomradeid=' + treeId, tree => {
     tree.soklista.sokdata.map(data => {
       data.foralder = treeId
-
-      elastic.save(data, 'proficiencies', 'proficiency')
-      afClient.doRequest('/af/v0/platsannonser/soklista/yrken?yrkesgruppid=' + data.id, yrken => {
-        yrken.soklista.sokdata.map(yrke => {
-          yrke.foralder = data.id
-          elastic.save(yrke, 'proficiencies', 'proficiency')
+      if (process.env.SUBPROFS || 'false' === 'true') {
+        elastic.save(data, 'proficiencies', 'proficiency').then(saved => {
+          afClient.doRequest('/af/v0/platsannonser/soklista/yrken?yrkesgruppid=' + data.id, yrken => {
+            yrken.soklista.sokdata.map(yrke => {
+              yrke.foralder = data.id
+              elastic.save(yrke, 'proficiencies', 'proficiency')
+            })
+          })
         })
-      })
+      }
     })
   })
 }
@@ -50,12 +52,27 @@ elastic.on('connect', () => {
   })
 
    /*
-    * Administration, Ekonomi och Juridik.
+    * Profs.
     */
   saveYrkeTree(1, 'Administration, Ekonomi och Juridik')
-
-   /*
-    * Hälso- och sjukvård.
-    */
+  saveYrkeTree(2, 'Bygg och anläggning')
+  saveYrkeTree(20, 'Chefer och verksamhetsledare')
+  saveYrkeTree(3, 'Data/IT')
+  saveYrkeTree(5, 'Försäljning, inköp, marknadsföring')
+  saveYrkeTree(6, 'Hantverksyrken')
+  saveYrkeTree(7, 'Hotell, restaurang, storhushåll')
   saveYrkeTree(8, 'Hälso-/sjukvård')
+  saveYrkeTree(9, 'Industriell tillverkning')
+  saveYrkeTree(10, 'Installation, drift, underhåll')
+  saveYrkeTree(4, 'Kropps- och skönhetsvård')
+  saveYrkeTree(11, 'Kultur, media, design')
+  saveYrkeTree(13, 'Naturbruk')
+  saveYrkeTree(14, 'Naturvetenskapligt arbete')
+  saveYrkeTree(15, 'Pedagogiskt arbete')
+  saveYrkeTree(12, 'Sanering och renhållning')
+  saveYrkeTree(16, 'Socialt arbete')
+  saveYrkeTree(17, 'Säkerhetsarbete')
+  saveYrkeTree(18, 'Tekniskt arbete')
+  saveYrkeTree(19, 'Transport')
+
 })
